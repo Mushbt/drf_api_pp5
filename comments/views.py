07b7333b5effb8fb1d27fdcc59387dc32b9e6a49
Mushbt <mustafa_habet@hotmail.com>
2023-11-26 17:56:02 +0000

    Replace regular class with generic views in comments views.py

--- comments/views.py
+++ comments/views.py
@@ -1,71 +1,18 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Comment
-from .serializers import CommentSerializer
+from rest_framework import generics, permissions
+from .serializers import CommentSerializer, CommentDetailSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
-class CommentList(APIView):
+class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
+    queryset = Comment.objects.all()
 
-    def get(self, request):
-        posts = Comment.objects.all()
-        serializer = CommentSerializer(
-            posts,
-            many=True,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = CommentSerializer(
-            data=request.data,
-            context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-class CommentDetail(APIView):
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CommentSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            post = Comment.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Comment.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = CommentSerializer(
-            post,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = CommentSerializer(
-            post,
-            data=request.data,
-            context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Comment.objects.all()
