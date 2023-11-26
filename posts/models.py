b9ef9047bf14b1dd1969f8da33e8df55a30ee245
Mushbt <mustafa_habet@hotmail.com>
2023-11-26 10:20:39 +0000

    Add post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.category
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    category = models.ManyToManyField(Category)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_etnxul', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
