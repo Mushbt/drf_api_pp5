da60b117cb8e40414a718587d1e23a902bf0b9a0
Mushbt <mustafa_habet@hotmail.com>
2023-11-26 13:49:28 +0000

    Add imports and commentList class to comments views.py file

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Comment
+from .serializers import CommentSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class CommentList(APIView):
+    serializer_class = CommentSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    
