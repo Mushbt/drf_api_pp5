8e24dd588b84bf8731737899da11796ad86299da
Mushbt <mustafa_habet@hotmail.com>
2023-11-26 18:06:05 +0000

    Correct intentation in comment views.py

--- comments/views.py
+++ comments/views.py
@@ -6,13 +6,15 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
-    ]
+        ]
     queryset = Comment.objects.all()
-
+    
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = CommentSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+        ]
     queryset = Comment.objects.all()
