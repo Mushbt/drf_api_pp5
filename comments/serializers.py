a435ed47ff7d8f6458f0d6da1493e73da61941b6
Mushbt <mustafa_habet@hotmail.com>
2023-11-26 18:32:24 +0000

    Fix typo in comments serializers.py file

--- comments/serializers.py
+++ comments/serializers.py
@@ -21,7 +21,7 @@ class CommentSerializer(serializers.ModelSerializer):
             'post',
             'created_on',
             'updated_on',
-            'comment_content',
+            'content',
             'profile_id',
             'profile_image',
         ]
