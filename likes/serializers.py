1eab38d07f35acfd3dea9bd7e86efc1f2f167fbf
Mushbt <mustafa_habet@hotmail.com>
2023-11-26 18:46:37 +0000

    Add like serializers

---
+++ likes/serializers.py
@@ -0,0 +1,16 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id',
+            'owner',
+            'created_on',
+            'post',
+        ]
