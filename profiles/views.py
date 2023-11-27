04531e880b58181d24fefc95d8a09e49e667816b
Mushbt <mustafa_habet@hotmail.com>
2023-11-27 18:00:10 +0000

    Fix typo in profiles views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import generics, status
+from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
