3a058eac3c09642599ef800131395694e3a3642a
Mushbt <mustafa_habet@hotmail.com>
2023-11-29 09:38:02 +0000

    Install django rest auth socials package

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -53,6 +53,11 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 
 
     'profiles',
@@ -62,6 +67,7 @@ INSTALLED_APPS = [
     'followers',
 ]
 
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -20,6 +20,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
