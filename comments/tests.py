598bbd48f7b2f3c10e9610cc5ae18b16e6bd867d
Mushbt <mustafa_habet@hotmail.com>
2023-12-01 22:05:35 +0000

    Add automated test to CommentListView

--- comments/tests.py
+++ comments/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Comment
+from posts.models import Post
 
-# Create your tests here.
+class CommentListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='samuel', password='password')
+    
+    def test_not_logged_in_user_cannot_create_comment(self):
+        response = self.client.post('/comments/', {'content': 'Amazing view!'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
