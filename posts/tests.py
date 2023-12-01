4c26b8a751a4f2a875d2183d858a698d1e3b6a90
Mushbt <mustafa_habet@hotmail.com>
2023-12-01 20:20:51 +0000

    Add automated testing for PostDetailView

--- posts/tests.py
+++ posts/tests.py
@@ -27,3 +27,37 @@ class PostListViewTests(APITestCase):
         Post.objects.create(owner=samuel, title='post title')
         response = self.client.get('/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class PostDetailViewTest(APITestCase):
+    def setUp(self):
+        samuel = User.objects.create_user(username='samuel', password='password')
+        angelo = User.objects.create_user(username='angelo', password='password')
+        Post.objects.create(
+            owner=samuel, title='post title',
+            description='view', country='Sweden'
+        )
+        Post.objects.create(
+            owner=angelo, title='post title2',
+            description='view2', country='Ireland'
+        )
+
+    def test_can_retrieve_existing_posts(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'post title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_can_not_retrieve_non_existing_posts(self):
+        response = self.client.get('/posts/300/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_user_can_delete_their_own_post(self):
+        self.client.login(username='samuel', password='password')
+        response = self.client.delete('/posts/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_user_can_not_delete_other_users_post(self):
+        self.client.login(username='samuel', password='password')
+        response = self.client.delete('/posts/2/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
