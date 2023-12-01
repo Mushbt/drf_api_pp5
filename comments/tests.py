450b4b3d5959bc79b28471595967a7ef3923e171
Mushbt <mustafa_habet@hotmail.com>
2023-12-01 22:25:30 +0000

    Set up initial  automated testcase for CommentDetailView

--- comments/tests.py
+++ comments/tests.py
@@ -11,3 +11,19 @@ class CommentListViewTests(APITestCase):
     def test_not_logged_in_user_cannot_create_comment(self):
         response = self.client.post('/comments/', {'content': 'Amazing view!'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class CommentDetailViewTests(APITestCase):
+    def setUp(self):
+        samuel = User.objects.create_user(username='samuel', password='password')
+        angelo = User.objects.create_user(username='angelo', password='password')
+        Post.objects.create(
+            owner=samuel, title='post title',
+            description='scenic', country='Lebanon')
+        Post.objects.create(
+            owner=angelo, title='post title 2',
+            description='wildlife', country='Brazil')
+        Comment.objects.create(owner=samuel, post_id=1, content='wow')
+        Comment.objects.create(owner=angelo, post_id=2, content='amazing')
+    
+    
