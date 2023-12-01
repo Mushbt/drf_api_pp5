f09db6e9e760f27385968691e6866743b1eeaab5
Mushbt <mustafa_habet@hotmail.com>
2023-12-01 19:00:07 +0000

    Add further testing for PostListView

--- posts/tests.py
+++ posts/tests.py
@@ -16,3 +16,14 @@ class PostListViewTests(APITestCase):
         self.client.login(username='samuel', password='password')
         response = self.client.post('/posts/', {'title': 'post title',
         'country': 'Lebanon'})
+
+    def test_post_must_include_all_fields(self):
+        self.client.login(username='samuel', password='password')
+        response = self.client.post('/posts/', {'country': 'Lebanon'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_user_can_view_posts(self):
+        samuel = User.objects.get(username='samuel')
+        Post.objects.create(owner=samuel, title='post title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
