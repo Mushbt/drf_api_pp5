715fe14af37385b3df07ee0836f481cb5d329b7f
Mushbt <mustafa_habet@hotmail.com>
2023-12-02 12:12:02 +0000

    Add automated test for FollowerListView

--- followers/tests.py
+++ followers/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Follower
 
-# Create your tests here.
+class FollowerListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='samuel', password='password')
+    
+    def test_not_logged_in_user_can_not_follow(self):
+        response = self.client.post('/followers/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
